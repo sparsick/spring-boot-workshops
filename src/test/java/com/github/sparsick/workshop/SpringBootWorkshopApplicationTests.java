7783977fda3ccfc740654eef0a924b3f9ff8195b
Sandra Parsick <sparsick@web.de>
2022-12-07 10:23:55 +0000

    init project

---
+++ src/test/java/com/github/sparsick/workshop/SpringBootWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sparsick.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
