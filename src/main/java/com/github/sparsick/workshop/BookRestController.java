431decd9f53ecc87ea3df7ef6a8c74773ba6ccd1
Sandra Parsick <sparsick@web.de>
2022-12-08 09:56:01 +0000

    feat: extend rest controller by pathvar and reqquest param

--- src/main/java/com/github/sparsick/workshop/BookRestController.java
+++ src/main/java/com/github/sparsick/workshop/BookRestController.java
@@ -6,7 +6,9 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -38,5 +40,25 @@ public class BookRestController {
     @GetMapping
     public List<Book> findAllBooks(){
         return this.books;
+    }
+
+    @GetMapping("/{isbn}")
+    public Book findSingleBook(@PathVariable String isbn){
+        return  this.books.stream().filter(book -> hasIsbn(book, isbn)).findFirst().orElseThrow();
+    }
+
+
+    @GetMapping(params = "author")
+    public Book searchBookbyAuthor(@RequestParam String author){
+        return this.books.stream().filter(book -> hasAuthor(book, author)).findFirst().orElseThrow();
+    }
+
+
+    private boolean hasIsbn(Book book, String isbn) {
+        return book.getIsbn().equals(isbn);
+    }
+
+    private boolean hasAuthor(Book book, String author) {
+        return book.getAuthor().contains(author);
     }
 }
