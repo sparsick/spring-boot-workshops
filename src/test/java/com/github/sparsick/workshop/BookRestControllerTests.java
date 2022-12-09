a751ddf264894fc826d0348f4b00df848cbcf15f
Sandra Parsick <sparsick@web.de>
2022-12-09 10:25:27 +0000

    test: add test variant with mockresult

--- src/test/java/com/github/sparsick/workshop/BookRestControllerTests.java
+++ src/test/java/com/github/sparsick/workshop/BookRestControllerTests.java
@@ -1,5 +1,7 @@
 package com.github.sparsick.workshop;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
@@ -7,9 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -17,6 +22,9 @@ public class BookRestControllerTests {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     void testFindAllBooks() throws Exception {
@@ -26,5 +34,17 @@ public class BookRestControllerTests {
             .andExpect(MockMvcResultMatchers.status().isOk())
             .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(3)))
             .andExpect(MockMvcResultMatchers.jsonPath("$[1].title", CoreMatchers.is("Clean Code")));
+    }
+
+    @Test
+    void testFindAllBooksWithJson() throws Exception {
+        // use static imports
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/book"))
+           .andReturn();
+        String jsonPayload = result.getResponse().getContentAsString();
+
+        Book[] books = objectMapper.readValue(jsonPayload, Book[].class);
+        assertEquals(3, books.length);
+        assertEquals("Clean Code", books[1].getTitle());
     }
 }
