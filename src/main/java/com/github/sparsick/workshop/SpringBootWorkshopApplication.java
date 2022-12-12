4f3a58a8f211f1f0b57f18582ca3d77e7edc99d0
Sandra Parsick <mail@sandra-parsick.de>
2022-12-12 10:22:33 +0100

    feat: swagger ui

--- src/main/java/com/github/sparsick/workshop/SpringBootWorkshopApplication.java
+++ src/main/java/com/github/sparsick/workshop/SpringBootWorkshopApplication.java
@@ -1,13 +1,32 @@
 package com.github.sparsick.workshop;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootWorkshopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWorkshopApplication.class, args);
+	}
+
+
+	@Bean
+	public OpenAPI api() {
+		return new OpenAPI()
+				.info(
+						new Info()
+								.title("Bookshelf API")
+								.version("v0.0.1")
+								.license(new License()
+										.name("MIT License")
+										.url("https://opensource.org/licenses/MIT")
+								)
+				);
 	}
 
 }
