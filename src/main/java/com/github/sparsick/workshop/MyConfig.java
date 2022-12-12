40c3fd5808092aa25543180be2e3cf33b4f4ddda
Sandra Parsick <mail@sandra-parsick.de>
2022-12-12 11:22:47 +0100

    chore: another config variante

---
+++ src/main/java/com/github/sparsick/workshop/MyConfig.java
@@ -0,0 +1,26 @@
+package com.github.sparsick.workshop;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfig {
+
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Bookshelf API")
+                                .version("v0.0.1")
+                                .license(new License()
+                                        .name("MIT License")
+                                        .url("https://opensource.org/licenses/MIT")
+                                )
+                );
+    }
+}
--- src/main/java/com/github/sparsick/workshop/SpringBootWorkshopApplication.java
+++ src/main/java/com/github/sparsick/workshop/SpringBootWorkshopApplication.java
@@ -1,11 +1,7 @@
 package com.github.sparsick.workshop;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootWorkshopApplication {
@@ -15,18 +11,6 @@ public class SpringBootWorkshopApplication {
 	}
 
 
-	@Bean
-	public OpenAPI api() {
-		return new OpenAPI()
-				.info(
-						new Info()
-								.title("Bookshelf API")
-								.version("v0.0.1")
-								.license(new License()
-										.name("MIT License")
-										.url("https://opensource.org/licenses/MIT")
-								)
-				);
-	}
+
 
 }
